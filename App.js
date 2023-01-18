d9b7c98a65fbff6283d9877b30f661887147bd71
aabdull1 <arnova.abdullah@smail.th-koeln.de>
2023-01-18 09:54:01 +0100

    Updated files are uploaded

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {Button, DrawerLayoutAndroidComponent, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import CameraScreen from './app/assets/Screen/camera';
 
-export default function App() {
+
+
+const myApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{ fontSize: 25 }}> Welcome to our </Text>
+      <Text style={{ fontSize: 30, textAlign: 'center' }}> Face Emotion Detection</Text>
+      <Text style={{ fontSize: 25 }}> App! </Text>
+      <Button
+        title="Go to Camera->"
+        color="#808080" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </View>
+    
+  );
+};
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome to Face Emotion Detection App!', textAlign: "center", alignItems: "center"}}
+        />
+        <Stack.Screen 
+          name="Camera" 
+          component={CameraScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+const HomeScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Go to Camera->"
+      onPress={() =>
+        navigation.navigate('CameraS')
+      }
+    />
+  );
+};
+const CameraS = ({navigation}) => {
+  return (
+    <CameraScreen />
+  );
+};
+
+
+export default MyStack;
